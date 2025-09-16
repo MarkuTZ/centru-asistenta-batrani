dfb7aee745c7e984fe5d4725ae70eea2f6df40d7
Mark Ciurea <ciureamark@yahoo.com>
2025-09-16 23:33:04 +0300

    mai mult continut pe pagini, styling

--- src/app/pages/acasa.page.ts
+++ src/app/pages/acasa.page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-acasa',
-  imports: [],
+  imports: [RouterLink],
   standalone: true,
   templateUrl: './acasa.page.html',
   styleUrl: './acasa.page.scss',
