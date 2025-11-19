29adfe33894d6f5cca06d128418d85d2b3e9605e
Mark Ciurea <79087763+MarkuTZ@users.noreply.github.com>
2025-11-19 22:21:58 +0200

    Add service, facilities, and resources pages

---
+++ src/app/pages/servicii.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicii',
+  standalone: true,
+  imports: [],
+  templateUrl: './servicii.page.html',
+  styleUrl: './servicii.page.scss',
+})
+export class ServiciiPage {}
